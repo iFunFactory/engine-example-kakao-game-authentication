23064644467d0dccc3a7ff13c0fa01dba8a5c2f3
Minwoo Seok <minwooseok@ifunfactory.com>
2017-06-22 14:40:40 +0900

    프로젝트 생성

---
+++ engine_example_kakao_game_authentication-source/src/engine_example_kakao_game_authentication_types.h
@@ -0,0 +1,16 @@
+﻿#ifndef SRC_ENGINE_EXAMPLE_KAKAO_GAME_AUTHENTICATION_TYPES_H_
+#define SRC_ENGINE_EXAMPLE_KAKAO_GAME_AUTHENTICATION_TYPES_H_
+
+#include <funapi.h>
+
+
+namespace engine_example_kakao_game_authentication {
+
+using fun::string;
+using fun::shared_ptr;
+using fun::weak_ptr;
+using fun::Uuid;
+
+}  // namespace engine_example_kakao_game_authentication
+
+#endif  // SRC_ENGINE_EXAMPLE_KAKAO_GAME_AUTHENTICATION_TYPES_H_
---
+++ engine_example_kakao_game_authentication-source/src/event_handlers.h
@@ -0,0 +1,20 @@
+﻿// PLEASE ADD YOUR EVENT HANDLER DECLARATIONS HERE.
+
+#ifndef SRC_EVENT_HANDLERS_H_
+#define SRC_EVENT_HANDLERS_H_
+
+#include <funapi.h>
+
+#include "engine_example_kakao_game_authentication_messages.pb.h"
+#include "engine_example_kakao_game_authentication_object.h"
+#include "engine_example_kakao_game_authentication_rpc_messages.pb.h"
+#include "engine_example_kakao_game_authentication_types.h"
+
+
+namespace engine_example_kakao_game_authentication {
+
+void RegisterEventHandlers();
+
+}  // namespace engine_example_kakao_game_authentication
+
+#endif  // SRC_EVENT_HANDLERS_H_
